7614a910606ccbba35627aa24a5792e75f5c89e4
paulmboyce <paul.boyce@goobito.com>
2021-04-16 15:59:33 +0100

    feature(landscape): use useWindowDimensions to control sizing.
    also: use width/height to decide if landscape.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
+import {
+	View,
+	StyleSheet,
+	Image,
+	Dimensions,
+	ScrollView,
+	useWindowDimensions,
+} from "react-native";
 
 import { Theme, ThemeStyles } from "../themes";
 import {
@@ -10,49 +17,55 @@ import {
 import ButtonPrimary from "../components/ButtonPrimary";
 
 const GameOverScreen = ({ numTries, onClickNewGame }) => {
+	const window = useWindowDimensions();
+	const landScape = window.width > window.height;
+	const imageSize = landScape ? window.height * 0.25 : window.width * 0.5;
+
+	const styles = StyleSheet.create({
+		windowSize: {
+			width: window.width,
+			height: window.height,
+		},
+		imageContainer: {
+			width: imageSize,
+			height: imageSize,
+			marginTop: landScape ? 20 : 100,
+			borderRadius: imageSize * 0.5,
+			borderWidth: 5,
+			borderColor: Theme.primaryColor,
+			overflow: "hidden",
+		},
+		image: {
+			width: "100%",
+			height: "105%",
+			resizeMode: "cover",
+		},
+		text: {
+			fontSize: 18,
+		},
+	});
 	return (
-		<ScrollView>
-			<View style={ThemeStyles.screen}>
-				<View style={ThemeStyles.box1}>
-					<View style={{ ...styles.imageContainer }}>
-						<Image
-							style={styles.image}
-							source={require("../assets/summit.png")}
-						/>
-					</View>
-					<ThemeTextTitle>
-						Game over in <ThemeTextHighlight>{numTries}</ThemeTextHighlight>{" "}
-						rounds!
-					</ThemeTextTitle>
+		<View style={{ ...ThemeStyles.screen, ...styles.windowSize }}>
+			<View style={ThemeStyles.box1}>
+				<View style={{ ...styles.imageContainer }}>
+					<Image
+						style={styles.image}
+						source={require("../assets/summit.png")}
+					/>
 				</View>
-				<View style={ThemeStyles.box1}>
-					<ThemeText style={styles.text}>Want to play again?</ThemeText>
-					<View style={{ paddingTop: 20 }}>
-						<ButtonPrimary onPress={onClickNewGame} title="PLAY AGAIN" />
-					</View>
+				<ThemeTextTitle>
+					Game over in <ThemeTextHighlight>{numTries}</ThemeTextHighlight>{" "}
+					rounds!
+				</ThemeTextTitle>
+			</View>
+			<View style={{ ...ThemeStyles.box1, justifyContent: "space-evenly" }}>
+				<ThemeText style={styles.text}>Want to play again?</ThemeText>
+				<View style={{ paddingTop: 20 }}>
+					<ButtonPrimary onPress={onClickNewGame} title="PLAY AGAIN" />
 				</View>
 			</View>
-		</ScrollView>
+		</View>
 	);
 };
 
-const styles = StyleSheet.create({
-	imageContainer: {
-		width: Dimensions.get("window").width * 0.5,
-		height: Dimensions.get("window").width * 0.5,
-		marginTop: 100,
-		borderRadius: Dimensions.get("window").width * 0.25,
-		borderWidth: 5,
-		borderColor: Theme.primaryColor,
-		overflow: "hidden",
-	},
-	image: {
-		width: "100%",
-		height: "105%",
-		resizeMode: "cover",
-	},
-	text: {
-		fontSize: 18,
-	},
-});
 export default GameOverScreen;
